4ca5335b63dfb4335e006843fe8699e8aac317dd
Carlos Eduardo Sepúlveda Toro <70768924+morpheusbelmont@users.noreply.github.com>
2026-07-20 11:26:31 -0500

    Create biblioteca.js

---
+++ biblioteca.js
@@ -0,0 +1,29 @@
+const biblioteca = {
+
+    marcoNormativo:[
+
+        {
+
+            titulo:"Constitución Política",
+
+            url:"...",
+
+            resumen:"...",
+
+            importancia:5
+
+        },
+
+        {
+
+            titulo:"Ley 115 de 1994",
+
+            url:"...",
+
+            importancia:5
+
+        }
+
+    ]
+
+};
